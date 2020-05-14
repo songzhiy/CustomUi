93bd0962edd580a8b3a3b6f3afa6b5d7f355b186
songzhiyang <406469533@qq.com>
2020-05-14 08:30:21 +0800

    [feature]:first commit

---
+++ touchedlightmodule/src/main/java/com/szy/touchedlightmodule/TouchedLightFrameLayout.kt
@@ -0,0 +1,32 @@
+package com.szy.touchedlightmodule
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import androidx.annotation.RequiresApi
+
+/**
+ * Created by songzhiyang on 2020-05-12.
+ * @author songzhiyang
+ */
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+class TouchedLightFrameLayout(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int
+) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0, 0)
+
+    constructor(context: Context) : this(context, null, 0, 0)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : this(
+        context,
+        attrs,
+        defStyleAttr,
+        0
+    )
+
+}
