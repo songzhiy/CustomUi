1d0b55d0b72f93e0963bc976fea45ab34af1af31
songzhiyang <406469533@qq.com>
2020-05-17 08:05:54 +0800

    [feature]:添加测试代码 & 尝试用假的点画一个月亮出来

--- touchedlightmodule/src/main/java/com/szy/touchedlightmodule/LightCustomView.kt
+++ touchedlightmodule/src/main/java/com/szy/touchedlightmodule/LightCustomView.kt
@@ -1,16 +1,22 @@
 package com.szy.touchedlightmodule
 
 import android.content.Context
+import android.graphics.*
 import android.os.Build
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.RequiresApi
+import kotlin.math.cos
+import kotlin.math.sin
 
 /**
  * Created by songzhiyang on 2020-05-15.
  * @author songzhiyang
  */
 class LightCustomView : View {
+
+    private var ratio = 0.5f
+    private val circleRadius = 100.0f
 
     constructor(context: Context) : this(context, null)
 
@@ -35,4 +41,84 @@ class LightCustomView : View {
         defStyleRes
     )
 
+    override fun onDraw(canvas: Canvas?) {
+        val paint = Paint()
+        val thisViewRectangle = Rect()
+        this.getDrawingRect(thisViewRectangle)
+        //1、画圆
+        drawCircle(canvas, thisViewRectangle, paint)
+        //2、寻找到两个圆上的点
+        val pairPoint = findTwoPoint(ratio, thisViewRectangle)
+        //3、画两个点的向心贝塞尔曲线 这个0.43是 oc = k * ab，其中 o为a,b中点，oc为ab垂线
+        drawInnerArc(pairPoint, 0.43f, canvas, paint)
+        //4、画两个点的离心弧线
+        //5、将圆与向心贝塞尔曲线、离心弧的区域进行裁剪，剩余的便是月亮
+    }
+
+    private fun drawInnerArc(
+        pairPoint: Pair<Point, Point>,
+        k: Float,
+        canvas: Canvas?,
+        paint: Paint
+    ) {
+        //todo 这里应该先通过k求出 两点中垂线上的点 这里先假设是0，0点
+        paint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.DST_OUT))
+        val pointC = getPointC(pairPoint, k)
+        //这时有了三个点，然后开始画向心弧线
+        val path = Path()
+        path.moveTo(pairPoint.second.x.toFloat(), pairPoint.second.y.toFloat())
+        path.cubicTo(
+            pairPoint.second.x.toFloat(), pairPoint.second.y.toFloat(),
+            pointC.x.toFloat(), pointC.y.toFloat(),
+            pairPoint.first.x.toFloat(), pairPoint.first.y.toFloat()
+        )
+        val rect = RectF()
+        rect.left = (pointC.x - circleRadius).toFloat()
+        rect.top = (pointC.y - circleRadius).toFloat()
+        rect.right = (pointC.x + circleRadius).toFloat()
+        rect.bottom = (pointC.y + circleRadius).toFloat()
+        path.addArc(rect, -45f, 90f)
+        canvas?.drawPath(path, paint)
+    }
+
+    private fun getPointC(pairPoint: Pair<Point, Point>, k: Float): Point {
+        val rect = Rect()
+        this.getDrawingRect(rect)
+        return Point(rect.centerX().toDouble(), rect.centerY().toDouble())
+    }
+
+    private fun findTwoPoint(ratio: Float, thisViewRectangle: Rect): Pair<Point, Point> {
+        val pointA = getPointA(ratio, thisViewRectangle)
+        val pointB = getPointB(ratio, thisViewRectangle)
+        return Pair(pointA, pointB)
+    }
+
+    private fun getPointA(ratio: Float, thisViewRectangle: Rect): Point {
+        //todo 两个点的位置应该与ratio相关
+        //pointA 应该从 -45度 -> 45度
+        val pointAX = thisViewRectangle.centerX() + cos(Math.PI / 4) * circleRadius
+        val pointAY = thisViewRectangle.centerY() - sin(-Math.PI / 4) * circleRadius
+        return Point(pointAX, pointAY)
+    }
+
+    private fun getPointB(ratio: Float, thisViewRectangle: Rect): Point {
+        //todo 两个点的位置应该与ratio相关
+        //pointB 应该从 -45度 -> -135度
+        val pointBX = thisViewRectangle.centerX() + cos(Math.PI / 4) * circleRadius
+        val pointBY = thisViewRectangle.centerY() - sin(Math.PI / 4) * circleRadius
+        return Point(pointBX, pointBY)
+    }
+
+    private fun drawCircle(
+        canvas: Canvas?,
+        thisViewRectangle: Rect,
+        paint: Paint
+    ) {
+        paint.color = Color.WHITE
+        val centerX = thisViewRectangle.centerX()
+        val centerY = thisViewRectangle.centerY()
+        canvas?.drawCircle(centerX.toFloat(), centerY.toFloat(), circleRadius, paint)
+    }
+
+    data class Point(val x: Double, val y: Double)
 }
--- touchedlightmodule/src/main/java/com/szy/touchedlightmodule/TouchedLightFrameLayout.kt
+++ touchedlightmodule/src/main/java/com/szy/touchedlightmodule/TouchedLightFrameLayout.kt
@@ -1,10 +1,21 @@
 package com.szy.touchedlightmodule
 
 import android.content.Context
+import android.content.res.Resources
 import android.os.Build
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import androidx.annotation.RequiresApi
+
+fun dp2px(context: Context?, dp: Float): Int {
+    var scale = 0f
+    if (context == null) {
+        scale = Resources.getSystem().displayMetrics.density
+    } else {
+        scale = context.resources.displayMetrics.density
+    }
+    return (scale * dp + 0.5).toInt()
+}
 
 /**
  * Created by songzhiyang on 2020-05-12.
@@ -19,6 +30,9 @@ class TouchedLightFrameLayout(
     defStyleAttr: Int,
     defStyleRes: Int
 ) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    //todo 天气view的直径 通过framelayout 自定义属性设置 处理给自定义view
+    private var lightViewRadius = dp2px(getContext(), 50f)
 
     constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0, 0)
 
